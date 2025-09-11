ecb525dc6f9c367d2d27f5f973ffe3549d5c1271
Rome <tgc@thegemchase.com>
2025-09-11 13:06:55 -0400

    h

--- src/autons.cpp
+++ src/autons.cpp
@@ -15,7 +15,7 @@ const int SWING_SPEED = 127;
 ///
 void default_constants() {
   // P, I, D, and Start I
-  chassis.pid_drive_constants_set(0.05, 0.0, 5.0);         // Fwd/rev constants, used for odom and non odom motions
+  chassis.pid_drive_constants_set(0.05, 0.0, 35.0);         // Fwd/rev constants, used for odom and non odom motions
   chassis.pid_heading_constants_set(11.0, 0.0, 20.0);        // Holds the robot straight while going forward without odom
   chassis.pid_turn_constants_set(3.0, 0.05, 20.0, 15.0);     // Turn in place constants
   chassis.pid_swing_constants_set(1.0, 0.0, 12.0);           // Swing constants
@@ -56,8 +56,8 @@ void drive_example() {
   // The second parameter is max speed the robot will drive at
   // The third parameter is a boolean (true or false) for enabling/disabling a slew at the start of drive motions
   // for slew, only enable it when the drive distance is greater than the slew distance + a few inches
-  chassis.pid_heading_constants_set(180);
-  chassis.pid_drive_set(24_in, 40, true);
+
+  chassis.pid_drive_set(24_in, 40);
   chassis.pid_wait();
 
   chassis.pid_drive_set(-12_in, 40);
