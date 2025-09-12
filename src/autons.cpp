d871d629c3f70a66957a172e9cb3e77ae4be5a88
Rome <tgc@thegemchase.com>
2025-09-12 14:19:15 -0400

    v

--- src/autons.cpp
+++ src/autons.cpp
@@ -383,7 +383,7 @@ void RedNeg() {
   chassis.odom_reset();
   pros::delay(250);
   LowerChannel.move(127);
-  chassis.pid_odom_set(-16, 20);
+  chassis.pid_odom_set(-16, 70);
   chassis.pid_wait();
   pros::delay(3000);
   //chassis.pid_turn_set(-78, 10);
