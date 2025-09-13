1317aa5e34c16b778422b7fa61aa5b1e033cccc5
Rome <tgc@thegemchase.com>
2025-09-13 12:03:55 -0400

    h

--- src/autons.cpp
+++ src/autons.cpp
@@ -17,7 +17,7 @@ void default_constants() {
   // P, I, D, and Start I
   chassis.pid_drive_constants_set(0.67, 0, 0);         // Fwd/rev constants, used for odom and non odom motions
   chassis.pid_heading_constants_set(200000, 0.0, 0.0);        // Holds the robot straight while going forward without odom
-  chassis.pid_turn_constants_set(10, 0, 0.0);     // Turn in place constants
+  chassis.pid_turn_constants_set(100000, 0, 0.0);     // Turn in place constants
   chassis.pid_swing_constants_set(1.0, 0.0, 12.0);           // Swing constants
   chassis.pid_odom_angular_constants_set(1200, 0.0, 2400);    // Angular control for odom motions
   chassis.pid_odom_boomerang_constants_set(5.8, 0.0, 32.5);  // Angular control for boomerang motions
