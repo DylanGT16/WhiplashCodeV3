0d50f3a711c8079afdb1235384c531557c17326b
Rome <tgc@thegemchase.com>
2025-09-13 08:29:54 -0400

    h

--- src/autons.cpp
+++ src/autons.cpp
@@ -15,7 +15,7 @@ const int SWING_SPEED = 127;
 ///
 void default_constants() {
   // P, I, D, and Start I
-  chassis.pid_drive_constants_set(-1600, 0, 0);         // Fwd/rev constants, used for odom and non odom motions
+  chassis.pid_drive_constants_set(-0.1, 0, -20);         // Fwd/rev constants, used for odom and non odom motions
   chassis.pid_heading_constants_set(-40, 0.0, 0.0);        // Holds the robot straight while going forward without odom
   chassis.pid_turn_constants_set(-40, 0, 0.0);     // Turn in place constants
   chassis.pid_swing_constants_set(1.0, 0.0, 12.0);           // Swing constants
@@ -385,9 +385,10 @@ void RedNeg() {
   chassis.slew_odom_reenable(true);
   pros::delay(250);
   LowerChannel.move(127);
-  chassis.pid_odom_set(4_in, 40, true);
+  chassis.pid_odom_set(16_in, 40, true);
   chassis.pid_wait();
   pros::delay(3000);
+  LowerChannel.move(0);
   //chassis.pid_turn_set(-78, 10);
   //chassis.pid_wait();
   //pros::delay(3000);
