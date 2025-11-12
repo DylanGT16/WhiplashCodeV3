20e9d144340e24a7d6d42327caeb2ca557f20de9
TGCreator <tgc@thegemchase.com>
2025-11-12 16:00:21 -0500

    g

--- src/autons.cpp
+++ src/autons.cpp
@@ -15,7 +15,7 @@ const int SWING_SPEED = 127;
 ///
 void default_constants() {
   // P, I, D, and Start I
-  chassis.pid_drive_constants_set(-1, 0.0, 0.0);        // Fwd/rev constants, used for odom and non odom motions
+  chassis.pid_drive_constants_set(-1.9, 0.0, 0.0);        // Fwd/rev constants, used for odom and non odom motions
   chassis.pid_heading_constants_set(1, 0.0, 0.0);        // Holds the robot straight while going forward without odom
   chassis.pid_turn_constants_set(1, 0, 0.0);     // Turn in place constants
   chassis.pid_swing_constants_set(1.0, 0.0, 12.0);           // Swing constants
