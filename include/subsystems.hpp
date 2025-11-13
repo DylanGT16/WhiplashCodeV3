5fcf59cf1da87baf53d4a064e636414416e47b90
TGCreator <tgc@thegemchase.com>
2025-11-13 16:11:55 -0500

    g

--- include/subsystems.hpp
+++ include/subsystems.hpp
@@ -7,8 +7,8 @@ extern Drive chassis;
 
 // Your motors, sensors, etc. should go here.  Below are examples
 
-inline pros::Motor LowerChannel(10);
-inline pros::Motor UpperChannel(7);
+inline pros::Motor LowerChannel(20);
+inline pros::Motor UpperChannel(12);
 inline ez::Piston MiddleGoalScore('A');
 inline ez::Piston LoaderIntake('B');
 inline ez::Piston HorizOdomUp('C');
