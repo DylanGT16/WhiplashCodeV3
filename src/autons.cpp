18d9d6a9f133ea69bf50ea8e948bca988cab88fa
TGCreator <tgc@thegemchase.com>
2025-12-04 16:32:18 -0500

    v

--- src/autons.cpp
+++ src/autons.cpp
@@ -448,8 +448,6 @@ void AWPRightStart() {
 };
 
 void Skills(){
-  chassis.pid_odom_set(36_in,40);
-  pros::delay(2000);
-  chassis.pid_turn_set(90,40);
+  chassis.pid_odom_set(10_in,40);
 
 };
--- src/main.cpp
+++ src/main.cpp
@@ -220,7 +220,7 @@ void ez_template_extras() {
       chassis.pid_tuner_toggle();
 
     // Trigger the selected autonomous routine
-    if (master.get_digital(DIGITAL_B)) {
+    if (master.get_digital(DIGITAL_LEFT)) {
       pros::motor_brake_mode_e_t preference = chassis.drive_brake_get();
       autonomous();
       chassis.drive_brake_set(preference);
