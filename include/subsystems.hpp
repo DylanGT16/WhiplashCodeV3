76623808322a7dcb44292885a109cc7bf78e81ce
TGCreator <tgc@thegemchase.com>
2026-01-29 16:18:22 -0500

    h

--- include/subsystems.hpp
+++ include/subsystems.hpp
@@ -7,7 +7,7 @@ extern Drive chassis;
 
 // Your motors, sensors, etc. should go here.  Below are examples
 
-inline pros::Motor Channel(18);
+inline pros::MotorGroup Channel({2,18});
 inline pros::Motor Scorer(13);
 inline ez::Piston MatchLoader('H');
 inline ez::Piston ScoreSwitcher('A');
